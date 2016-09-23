c0be02a623aad5fed31510d5e5e01360b6759434
Peter Snider <snider.peter@gmail.com>
2016-09-22 17:53:02 -0700

    works in people-service

--- document-database-if.d.ts
+++ document-database-if.d.ts
@@ -1,5 +1,6 @@
 type Promise<T> = any
 
+type DatabaseID = string;
 
 export interface Cursor {
     start_offset?:  number
@@ -17,58 +18,61 @@ export interface UpdateFieldCommand {
 }
 
 
+type Conditions = {}
+type Fields = string[]
+// specify the field on which to sort, with a value of 1 meaning ascending, and -1 meaning descending
+type Sort = {[fieldname: string]: number}
+
+
 export interface RequestQuery {
     ids?:           string[];   // DatabaseObjectID
-    conditions?:    Object
-    fields?:        string[]
-    sort?:          Object
+    conditions?:    Conditions
+    fields?:        Fields
+    sort?:          Sort
     cursor?:        Cursor
 }
 
 
+type Action = 'create' | 'read' | 'update' | 'delete' | 'find'
+
 export interface Request<T> {
-    action:         string
+    action:         Action
     obj?:           T
     query?:         RequestQuery
     updates?:       UpdateFieldCommand[]
 }
 
 
-export interface Response_Result<T> {
-    total_count?: number
-    elements: T[]
-}
-
-
 export interface Response<T> {
     error?: any
-    result?: Response_Result<T>
+    total_count?: number
+    elements?: T[]
 }
 
 
-export interface ResponseWStatus<T> {
-    http_status?:   number
-    response?:      Response<T>
-}
 
-// type place-holders until mongoose.d.ts defines these
-type Conditions = {}
-type Fields = {}
-type Sort = {}
+
+type CreateCallback<T>   = (error: Error, result?: T) => void
+type ReadCallback<T>      = (error: Error, result?: T) => void
+type UpdateSingleCallback<T> = (error: Error, result?: T) => void
+type DeleteSingleCallback = (error?: Error) => void
+type FindCallback<T> = (error: Error, results?: T[]) => void
+type GetOriginalDocumentCallback<T> = (doc : T) => void
 
 
 // Calls return either:
 // - void: if a callback is provided
 // - a Promise: if a callback is not provided
-export interface DocumentDatabase<T> {
-    create(obj : T) : Promise<T>
-    create(obj : T, done: (error: Error, result?: T) => void) : void
+export abstract class DocumentDatabase<T> {
+    constructor(id: string, typename: string)
+    create(obj: T): Promise<T>
+    create(obj: T, done: CreateCallback<T>): void
     read(id : string) : Promise<T>
-    read(id : string, done: (error: Error, result?: T) => void) : void
-    update(conditions : Conditions, updates: UpdateFieldCommand[], getOriginalDocument?: (doc : T) => void) : Promise<T>
-    update(conditions : Conditions, updates: UpdateFieldCommand[], getOriginalDocument: (doc : T) => void, done: (error: Error, result?: T) => void) : void
-    delete(conditions : Conditions, getOriginalDocument?: (doc : T) => void) : Promise<void>
-    delete(conditions : Conditions, getOriginalDocument: (doc : T) => void, done: (error: Error) => void) : void
+    read(id : string, done: ReadCallback<T>) : void
+    update(conditions : Conditions, updates: UpdateFieldCommand[], getOriginalDocument?: GetOriginalDocumentCallback<T>) : Promise<T>
+    update(conditions : Conditions, updates: UpdateFieldCommand[], getOriginalDocument: GetOriginalDocumentCallback<T>, done: UpdateSingleCallback<T>) : void
+    del(conditions : Conditions, getOriginalDocument?: (doc : T) => void) : Promise<void>
+    del(conditions : Conditions, getOriginalDocument: (doc : T) => void, done: DeleteSingleCallback) : void
     find(conditions : Conditions, fields?: Fields, sort?: Sort, cursor?: Cursor) : Promise<T[]> 
-    find(conditions : Conditions, fields: Fields, sort: Sort, cursor: Cursor, done: (error: Error, result?: T[]) => void) : void
+    find(conditions : Conditions, fields: Fields, sort: Sort, cursor: Cursor, done: FindCallback<T>) : void
 }
