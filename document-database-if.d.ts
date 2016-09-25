c69e3563eaf47b370ae522564ad1a559d594f213
Peter Snider <snider.peter@gmail.com>
2016-09-25 13:14:15 -0700

    use _id consistently, make interface more consistent

--- document-database-if.d.ts
+++ document-database-if.d.ts
@@ -54,27 +54,28 @@ export interface Response<T> {
 type ErrorOnlyCallback = (error?: Error) => void
 type ObjectCallback<T>   = (error: Error, result?: T) => void
 type ArrayCallback<T> = (error: Error, results?: T[]) => void
+type ObjectOrArrayCallback<T> = (error: Error, results?: T | T[]) => void
 
 
 // Calls return either:
 // - void: if a callback is provided
 // - a Promise: if a callback is not provided
 export abstract class DocumentDatabase<T> {
-    constructor(id: string, type: string | {})
+    constructor(db_name: string, type: string | {})
     connect(done: ErrorOnlyCallback): void
     connect() : Promise<void>
     disconnect(done: ErrorOnlyCallback): void
     disconnect() : Promise<void>
     create(obj: T): Promise<T>
     create(obj: T, done: ObjectCallback<T>): void
-    read(id : string) : Promise<T>
-    read(id : string, done: ObjectCallback<T>) : void
+    read(_id_or_ids: DatabaseID | DatabaseID[]) : Promise<T | T[]> 
+    read(_id_or_ids: DatabaseID | DatabaseID[], done: ObjectOrArrayCallback<T>) : void
     replace(obj: T) : Promise<T>
     replace(obj: T, done: ObjectCallback<T>) : void
-    update(conditions : Conditions, updates: UpdateFieldCommand[], getOriginalDocument?: ObjectCallback<T>) : Promise<T>
-    update(conditions : Conditions, updates: UpdateFieldCommand[], getOriginalDocument: ObjectCallback<T>, done: ObjectCallback<T>) : void
-    del(conditions : Conditions, getOriginalDocument?: (doc : T) => void) : Promise<void>
-    del(conditions : Conditions, getOriginalDocument: (doc : T) => void, done: ErrorOnlyCallback) : void
+    update(conditions : Conditions, updates: UpdateFieldCommand[]) : Promise<T>
+    update(conditions : Conditions, updates: UpdateFieldCommand[], done: ObjectCallback<T>) : void
+    del(_id: DatabaseID) : Promise<void>
+    del(_id: DatabaseID, done: ErrorOnlyCallback) : void
     find(conditions : Conditions, fields?: Fields, sort?: Sort, cursor?: Cursor) : Promise<T[]> 
     find(conditions : Conditions, fields: Fields, sort: Sort, cursor: Cursor, done: ArrayCallback<T>) : void
 }
